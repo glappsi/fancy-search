823b92b319f829a69df5df80b7038cb9d6afebf8
Florian Glappa <glappa.js@gmail.com>
2019-10-09 22:20:11 +0200

    exclude rxjs

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,11 +12,16 @@ const libraryName = 'fancy-search'
 export default {
   input: `src/${libraryName}.ts`,
   output: [
-    { file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true },
+    {
+      file: pkg.main,
+      name: camelCase(libraryName),
+      format: 'umd',
+      sourcemap: true
+    },
     { file: pkg.module, format: 'es', sourcemap: true }
   ],
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: ['rxjs', 'rxjs/operators', '@aurally/speech-control'],
   watch: {
     include: 'src/**'
   },
